24a1abad366f06858f35a11696064df6a48925d9
myy <myy@123456.com>
2025-08-20 21:05:52 +0800

    master commit2

--- src/main.java
+++ src/main.java
@@ -4,5 +4,6 @@ public class main {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master commit");
     }
 }
